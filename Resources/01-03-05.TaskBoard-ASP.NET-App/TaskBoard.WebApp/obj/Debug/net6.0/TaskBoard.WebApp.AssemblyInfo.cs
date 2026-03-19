54fd82d206996cafba12b47606dcf63bbbcf9033
LPanov <lubomirpanov99@gmail.com>
2026-03-19 21:38:05 +0200

    Finished 4th, 5th, 6th and 8th exercise

--- Resources/01-03-05.TaskBoard-ASP.NET-App/TaskBoard.WebApp/obj/Debug/net6.0/TaskBoard.WebApp.AssemblyInfo.cs
+++ Resources/01-03-05.TaskBoard-ASP.NET-App/TaskBoard.WebApp/obj/Debug/net6.0/TaskBoard.WebApp.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskBoard.WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+826ac1962d1a79dc914f12d1acb85f7a13afe6b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskBoard.WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskBoard.WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
